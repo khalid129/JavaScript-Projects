cfd2508d15566e530cea3bcb79930ab4a734ad67
Muhammad Khalid <muhammadkhalid.ch129@gmail.com>
2021-02-11 13:10:28 +0500

    Quiz App has been completed

--- QuizApps/index.js
+++ QuizApps/index.js
@@ -1,6 +1,6 @@
 const quizData = [
     {
-        question : 'Top Programming languages in 2021',
+        question : 'Top Programming languages in 2021 ?',
         a: 'JavaScript',
         b: 'Python',
         c: 'C#',
@@ -16,7 +16,7 @@ const quizData = [
         answer: 'b'
     },
     {
-        question : 'Which is most popular web browser',
+        question : 'Which is most popular web browser ?',
         a: 'Internet Explorer',
         b: 'Opera',
         c: 'Chrome',
@@ -24,7 +24,7 @@ const quizData = [
         answer: 'c'
     },
     {
-        question : 'Best Language for Data Science',
+        question : 'Best Language for Data Science ?',
         a: 'JavaScript',
         b: 'Kotlin',
         c: 'R Language',
@@ -32,7 +32,7 @@ const quizData = [
         answer: 'd'
     },
     {
-        question : 'Which framework provide the corss platform mobile app',
+        question : 'Which framework provide the corss platform mobile app ?',
         a: 'React Js',
         b: 'Redux',
         c: 'React Native',
@@ -40,7 +40,7 @@ const quizData = [
         answer: 'c'
     },
     {
-        question : '.Net-core Framework used for',
+        question : '.Net-core Framework used for ?',
         a: 'Web Apps',
         b: 'Desktop app',
         c: 'Console app',
@@ -48,7 +48,7 @@ const quizData = [
         answer: 'a'
     },
     {
-        question : 'Which Operating system is Open source',
+        question : 'Which Operating system is Open source ?',
         a: 'Window',
         b: 'Linux',
         c: 'Mac OS',
@@ -56,7 +56,8 @@ const quizData = [
         answer: 'b'
     }
 ]
-
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.getElementById("question");
 const a_text = document.getElementById("a_text");
 const b_text = document.getElementById("b_text");
@@ -65,11 +66,11 @@ const d_text = document.getElementById("d_text");
 const submitBtn = document.getElementById("submit");
 
 let currentQuiz = 0; 
-
+let score = 0;
+let smile = undefined;
 Quizload();
 
 function Quizload(){
-
     let currentQuizQuestion = quizData[currentQuiz];
     questionEl.innerText = currentQuizQuestion.question;
     a_text.innerText = currentQuizQuestion.a;
@@ -78,16 +79,60 @@ function Quizload(){
     d_text.innerText = currentQuizQuestion.d;
 }
 
+function getSelected(){
+    let answer = undefined;
+
+    answerEls.forEach((answerEl)=>{
+        if(answerEl.checked){
+            answer =  answerEl.id;
+        }
+    })
+    return answer;
+}
+
+function deseclectedAnswer(){
+    answerEls.forEach((answerEl)=>{
+            answerEl.checked=false;
+    })
+}
 
 submitBtn.addEventListener('click',()=>{
     
-    if(currentQuiz<quizData.length){
-    Quizload();
+    const answer = getSelected();
+    deseclectedAnswer();
+
+    if(answer){
+        if (answer === quizData[currentQuiz].answer){
+            score++;
+        }
     currentQuiz++;
-    }
-    else{
-        alert('Quiz HAs Been finished');
-        currentQuiz =0;
+
+    if(currentQuiz<quizData.length){
         Quizload();
+    }
+    // else if(currentQuiz<=quizData.length){
+    //     // quiz.innerHTML = `<h2>You Answered corecctly at ${score}/${quizData.length} </h2>`
+    //     if (score>0 && score<4){
+    //         alert("You have been faild 😢 Your Score is "+score+" Try Again" );
+    //     }
+    //     else if(score>3 && score<=7){
+    //         alert("You have been Passed 😃 Your Score is "+score )
+    //     }
+    //     currentQuiz =0;
+    //     Quizload();
+    // }
+    else{
+        if(score>0 && score<4){
+             smile = '😢';
+        }
+        else if(score>3 && score <=7){
+             smile = '😃';
+        }
+        quiz.innerHTML = `<h2>You Answered corecctly at ${score}/${quizData.length} Questions  
+        ${smile}</h2><button onClick = "location.reload()">Reload</button>`
+    }
+}
+    else{
+        alert("Please Select an Answer");
     }
 })
